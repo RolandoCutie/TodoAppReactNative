0deaf7ee343251857ee5c968c30d76a55ce2b2e4
Rolando Garcia Cutie <rolando.garcia@alivi.com>
2025-01-23 18:24:36 -0500

    a;adir configuracion para forzar solo uso de typescript

---
+++ .history/App_20250123165029.tsx
@@ -0,0 +1,108 @@
+import { AntDesign } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { FlatList, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import TodoList from './components/TodoList';
+import { Colors } from './constants/colors';
+import tempData from './tempData';
+import { AddListModal } from './components/AddListModal';
+
+export default function App() {
+    const [modalVisible, setModalVisible] = useState(false);
+
+
+    const toggleAddTodoModal = () => {
+        setModalVisible(!modalVisible);
+    };
+
+    renderList=list=>{
+        return(
+            
+        )
+    }
+    debugger;
+    return (
+        <View style={styles.container}>
+            <Modal visible={modalVisible} animationType="slide" >
+                <View style={styles.modalContent}>
+                    <AddListModal closeModal={toggleAddTodoModal} />
+                </View>
+            </Modal>
+
+            <View style={{ flexDirection: 'row' }}>
+                <View style={styles.divider} />
+                <Text style={styles.title}>
+                    Todo <Text style={{ fontWeight: '300', color: Colors.blue }}>Lists</Text>
+                </Text>
+                <View style={styles.divider} />
+            </View>
+
+            <View style={{ marginVertical: 48 }}>
+                <TouchableOpacity style={styles.addList} onPress={toggleAddTodoModal}>
+                    <AntDesign name="plus" size={16} color={Colors.blue} />
+                </TouchableOpacity>
+                <Text style={styles.add}>Add List</Text>
+            </View>
+
+            <View style={{ height: 275, paddingLeft: 32 }}>
+                <FlatList
+                    data={tempData}
+                    keyExtractor={(item) => item.name}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item }) => <TodoList list={item} />}
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    divider: {
+        backgroundColor: Colors.lightBlue,
+        height: 1,
+        flex: 1,
+        alignSelf: 'center',
+    },
+    title: {
+        fontSize: 38,
+        fontWeight: '800',
+        color: Colors.black,
+        paddingHorizontal: 64,
+    },
+    addList: {
+        borderWidth: 2,
+        borderColor: Colors.lightBlue,
+        borderRadius: 4,
+        padding: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    add: {
+        color: Colors.blue,
+        fontWeight: '800',
+        fontSize: 14,
+        marginTop: 8,
+    },
+    modalContent: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.white,
+    },
+    closeModalButton: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: Colors.blue,
+        borderRadius: 5,
+    },
+    closeModalText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+});
---
+++ .history/components/TodoList_20250123171411.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from '../constants/colors';
+
+
+// Define the type for the list item that will be passed to the TodoList function wich will be
+//responsable for obtain the data and showed to the user
+interface List {
+    name: string;
+    color: string;
+    todos: Todo[];
+}
+
+interface Todo {
+    name: string;
+    completed: boolean;
+}
+//This will be what we call in flutter widget that will be the responsable for display some
+//data
+
+export default class TodoList extends React.Component<{ list: List }> {
+
+
+
+    render() {
+        const [showListVisible, setModalVisible] = useState(false);
+
+        const toggleAddTodoModal = () => {
+            setModalVisible(!modalVisible);
+        };
+        const list = this.props.list;
+        const completedTodos = list.todos.filter((todo) => todo.completed).length;
+        const remainigTodos = list.todos.length - completedTodos;
+        return (
+            <View style={[styles.listContainer, { backgroundColor: list.color, }]}>
+                <Text style={styles.title} numberOfLines={2}>{list.name}</Text>
+
+                <View>
+                    <View style={{ alignItems: "center" }}>
+                        <Text style={styles.completed}>{remainigTodos}</Text>
+                        <Text style={styles.remainign}>Remainign</Text>
+                    </View>
+                    <View style={{ alignItems: "center" }}>
+                        <Text style={styles.completed}>{completedTodos}</Text>
+                        <Text style={styles.remainign}>Completed</Text>
+                    </View>
+                </View>
+
+            </View >
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    listContainer: {
+        paddingVertical: 34,
+        paddingHorizontal: 16,
+        borderRadius: 6,
+        marginHorizontal: 12,
+        alignItems: 'center',
+        width: 200
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: '700',
+        color: Colors.white,
+        marginBottom: 18
+    },
+    completed: {
+        fontSize: 48,
+        fontWeight: '200',
+        color: Colors.white,
+
+    },
+    remainign: {
+        fontSize: 12,
+        fontWeight: '700',
+        color: Colors.white,
+
+    },
+});
